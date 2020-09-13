5990eba9cdb23ec7e8cf1c9b098f430e4e6c1d7e
fingerskier <fingerskier@gmail.com>
2020-09-13 16:05:12 -0500

    add build

---
+++ build/precache-manifest.cfb4cdcdd79496b5829e7b8ba06637f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0581710211aafde70f3815e087c555d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e86fc75b53bbda931a6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7201683901b0cca7dc1a",
+    "url": "/static/js/2.979b307e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.979b307e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e86fc75b53bbda931a6",
+    "url": "/static/js/main.96e6fb38.chunk.js"
+  },
+  {
+    "revision": "595ada3cec3b7ef8ee53",
+    "url": "/static/js/runtime-main.28d2e7a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
