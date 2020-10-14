23279b90b7f7beeee591006815f48e3ca8cde286
fingerskier <fingerskier@gmail.com>
2020-10-14 18:19:38 -0500

    Updates

--- precache-manifest.84438bd2b78d3969a86ec685b40ad5c4.js
+++ precache-manifest.84438bd2b78d3969a86ec685b40ad5c4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9c2663b9041495f22a8aeb1ece1b8567",
+    "revision": "e83c71c8e3046257b0e6e4709fabca3f",
     "url": "/envelope/index.html"
   },
   {
-    "revision": "0ebe9ee0dd095bc8d86e",
-    "url": "/envelope/static/css/main.58cf69b5.chunk.css"
+    "revision": "9d686766387404cd3f0e",
+    "url": "/envelope/static/css/main.415a6734.chunk.css"
   },
   {
     "revision": "b38892a82ecb228e07ea",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/envelope/static/js/2.0da2a9ed.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0ebe9ee0dd095bc8d86e",
-    "url": "/envelope/static/js/main.c074662a.chunk.js"
+    "revision": "9d686766387404cd3f0e",
+    "url": "/envelope/static/js/main.fb9d65f1.chunk.js"
   },
   {
     "revision": "86338c0b9e5dd6943306",
