be846d4bb306e3290c1bd426302afd13559f7d03
fingerskier <fingerskier@gmail.com>
2021-02-20 10:36:16 -0600

    verse-number tooltips

--- src/Chapter.js
+++ src/Chapter.js
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-function Chapter({chapter}) {
-	function Verse({index,verse}) {
-		return <span
-			data-index={index}
-			key={index}
-		>
-			{verse}&nbsp;&nbsp;
-		</span>
+function Chapter({ chapter }) {
+	function Verse({ index, verse }) {
+		const tooltipId = `tooltip_${chapter}_${verse}`
+
+		function hideTooltip(event) {
+			document.getElementById(tooltipId).style.display = 'none'
+		}	
+		function showTooltip(event) {
+			document.getElementById(tooltipId).style.display = 'inline'
+		}	
+
+		return <>
+			<span className="verse tooltip" id={tooltipId}>{index}</span>
+			<span
+				data-index={index}
+				key={index}
+				onMouseOut={hideTooltip}
+				onMouseOver={showTooltip}
+				onClick={showTooltip}
+			>
+				{verse}&nbsp;&nbsp;
+			</span>
+		</>
 	}
 
 	function Verses() {
 		if (chapter) {
-			return chapter.map((el,i)=>{
+			return chapter.map((el, i) => {
 				if (i) return <Verse index={i} key={i} verse={el} />
 			})
 		} else {
 			return <></>
 		}
 	}
+
 
 	return (
 		<div>
